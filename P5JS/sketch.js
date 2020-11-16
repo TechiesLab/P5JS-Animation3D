aaa5c074cd7f7e28be77045eb7bb3daa88691d61
DanielCortild <daniel.cortild@gmail.com>
2020-11-16 17:18:04 +0100

    Problème de Couleurs

--- P5JS/sketch.js
+++ P5JS/sketch.js
@@ -6,10 +6,12 @@ function draw() {
   background(220);
   rotateX(1);
   translate(-180, -180);
+  normalMaterial();
   for (let l = 0; l < 36; l += 1) {
     for (let c = 0; c < 36; c += 1) {
       let offset = dist(l, c, 18, 18) / 5;
-      box(10, 10, (sin(millis() / 1000 + offset) + 1) * 100);
+      let h = (sin(millis() / 1000 + offset) + 1) * 100;
+      box(10, 10, h);
       translate(10, 0);
     }
     translate(-360, 10);
